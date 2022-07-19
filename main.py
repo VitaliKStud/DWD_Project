1e6648b449eaf20f1b9851c1a24f904966c17466
VitaliK <vitali.krilov002@stud.fh-dortmund.de>
2022-07-19 09:54:54 +0200

    doin

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import os
 # from DwdDataScrapper import DataScrapper
 # from DwdDataPrep import Reader
 #
-#testdd
+#testddrtt
 #
 # looking_for_ = ["FF_10"]
 # # choose your data you need to plot #Check DwdDict
